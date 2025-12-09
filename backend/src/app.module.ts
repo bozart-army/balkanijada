eff99ca8b3030f5a122cab74b0221f1af6291e2f
Bozart <bagaricmail@gmail.com>
2025-12-09 11:25:35 +0100

    Wire PlacesModule into AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,19 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PlacesModule } from './modules/places/places.module';
-import { EventsModule } from './modules/events/events.module';
-import { StoriesModule } from './modules/stories/stories.module';
-import { UsersModule } from './modules/users/users.module';
-import { OrganizersModule } from './modules/organizers/organizers.module';
-import { I18nModule } from './modules/i18n/i18n.module';
 
 @Module({
-  imports: [
-    PlacesModule,
-    EventsModule,
-    StoriesModule,
-    UsersModule,
-    OrganizersModule,
-    I18nModule,
-  ],
+  imports: [PlacesModule],
 })
 export class AppModule {}
